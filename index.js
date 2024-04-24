245cdf9dd1245eb33ea083e7d46ce6a25adca4d6
MCServPro <134057590+mcservpro@users.noreply.github.com>
2024-04-25 00:35:51 +0300

    update fix
    By Xarvetel

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
     toggleThemeBtn.addEventListener('click', toggleTheme);
 
     function toggleTheme() {
-        const isDarkTheme = document.body.classList.contains('dark-theme');
+        const isDarkTheme = document.body.classList.contains('light-theme');
 
         if (!isDarkTheme) {
-            document.body.classList.add('dark-theme');
+            document.body.classList.add('light-theme');
         } else {
-            document.body.classList.remove('dark-theme');
+            document.body.classList.remove('light-theme');
         }
     }
 });
+
+document.getElementById('theme').addEventListener('click', function() {
+    var themeButton = document.getElementById('theme');
+    var themeIcon = themeButton.querySelector('span.material-symbols-outlined');
+
+    if (themeIcon.textContent === 'light_mode') {
+      themeIcon.textContent = 'dark_mode';
+    } else {
+      themeIcon.textContent = 'light_mode';
+    }
+  });
